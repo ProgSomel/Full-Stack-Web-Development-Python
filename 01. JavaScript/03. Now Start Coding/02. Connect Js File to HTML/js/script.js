e3980fcde1012741ac1022097521f3819183a158
Somel <somelahmed88@gmail.com>
2023-05-30 23:25:22 +0600

    Now Start Coding Added

---
+++ 01. JavaScript/03. Now Start Coding/02. Connect Js File to HTML/js/script.js
@@ -0,0 +1,5 @@
+window.alert('I am Learning JavaScript');
+alert('I am Learning JavaScript'); //?Both will work as same
+document.write("I am Learning JavaScript");
+        
+console.log("I love you Saima");
